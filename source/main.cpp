2015a1bea1844a047af7dc45e041ec2a2e21824a
Romanas <gensatus@gmail.com>
2021-04-02 21:20:58 +0300

    https://youtu.be/GAi_nTx1zG8?t=4344

--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,34 @@
 #include <windows.h>
-// https://youtu.be/GAi_nTx1zG8?t=1842
-#define static_function static
-#define static_global static
-#define static_local static
+// https://youtu.be/GAi_nTx1zG8?t=4344
 
-static_global auto GameRunning = true;
+static auto Global_GameRunning = true;
+
+static BITMAPINFO Global_BitmapInfo;
+static void *Global_BitmapMemory;
+static HBITMAP Global_BitmapHandle;
+static HDC Global_BitmapDeviceContext;
 
 // DIB - device independant section
 static void Win32ResizeDIBSection(int width, int height) {
-  
+
+  if (Global_BitmapHandle)
+    DeleteObject(Global_BitmapHandle);
+
+  if (!Global_BitmapDeviceContext)
+    Global_BitmapDeviceContext = CreateCompatibleDC(0);
+
+  Global_BitmapInfo.bmiHeader.biSize = sizeof(Global_BitmapInfo.bmiHeader);
+  Global_BitmapInfo.bmiHeader.biWidth = width;
+  Global_BitmapInfo.bmiHeader.biHeight = height;
+  Global_BitmapInfo.bmiHeader.biPlanes = 1;
+  Global_BitmapInfo.bmiHeader.biBitCount = 32;
+  Global_BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+  Global_BitmapHandle = CreateDIBSection(Global_BitmapDeviceContext, &Global_BitmapInfo, DIB_RGB_COLORS, &Global_BitmapMemory, 0, 0);
 }
 
-static void Win32UpdateWindow(HWND window, int x, int y, int width, int height) {
-  
+static void Win32UpdateWindow(HDC deviceContext, int x, int y, int width, int height) {
+  StretchDIBits(deviceContext, x, y, width, height, x, y, width, height, Global_BitmapMemory, &Global_BitmapInfo, DIB_RGB_COLORS, SRCCOPY);
 }
 
 LRESULT CALLBACK Win32WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -27,10 +43,10 @@ LRESULT CALLBACK Win32WindowProc(HWND window, UINT message, WPARAM wParam, LPARA
     Win32ResizeDIBSection(width, height);
   } break;
   case WM_DESTROY: {
-    GameRunning = false;
+    Global_GameRunning = false;
   } break;
   case WM_CLOSE: {
-    GameRunning = false;
+    Global_GameRunning = false;
   } break;
   case WM_ACTIVATEAPP: {
     OutputDebugStringA("WM_ACTIVATEAPP\n");
@@ -44,7 +60,7 @@ LRESULT CALLBACK Win32WindowProc(HWND window, UINT message, WPARAM wParam, LPARA
     auto width = paint.rcPaint.right - paint.rcPaint.left;
     auto height = paint.rcPaint.bottom - paint.rcPaint.top;
     
-    Win32UpdateWindow(window, x, y, width, height);
+    Win32UpdateWindow(deviceContext, x, y, width, height);
     EndPaint(window, &paint);
   } break;
   default: {
@@ -75,7 +91,7 @@ int main(HINSTANCE currentInstance, HINSTANCE previousInstance, LPSTR commandLin
 
   MSG message;
   BOOL windowMessage;
-  while (GameRunning) {
+  while (Global_GameRunning) {
     windowMessage = GetMessage(&message, 0, 0, 0);
 
     if (windowMessage < 0)
