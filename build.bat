19659049e4c14b32627596e7fe3ae7edd6619f6a
mtxset <gensatus@gmail.com>
2021-07-08 00:17:40 +0300

    end of day 10 (#8)

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 
 mkdir build
 pushd build
-:: call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 cl.exe /W3 -FC -Zi -nologo "..\source\main.cpp" user32.lib gdi32.lib
 popd
