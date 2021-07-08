0118ccf00ce2ccb555589cc4c9858516fd6a1dfc
Romanas <gensatus@gmail.com>
2021-07-09 00:18:00 +0300

    end of day 11

--- build.bat
+++ build.bat
@@ -5,3 +5,5 @@ pushd build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 cl.exe /W3 -FC -Zi -nologo "..\source\main.cpp" user32.lib gdi32.lib
 popd
+
+:: cl -some_preprocessor_param=1
