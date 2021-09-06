67636103cb173f0f00b1f597b419151589c9c437
mtxset <gensatus@gmail.com>
2021-09-06 19:37:59 +0300

    end of day 26 (#24)

--- source/utils.cpp
+++ source/utils.cpp
@@ -1,21 +1,19 @@
 #include <stdint.h>
 #include "types.h"
 
-template <typename T> void 
-swap(T& a, T& b) {
+template <typename T> 
+void swap(T& a, T& b) {
     T c(a);
     a = b;
     b = c;
 }
 
-u32 
-truncate_u64(u64 value) {
+u32 truncate_u64(u64 value) {
     macro_assert(value <= UINT32_MAX);
     return (u32)value;
 }
 
-void
-string_concat(size_t src_a_count, char* src_a, size_t src_b_count, char* src_b, size_t dest_count, char* dest) {
+void string_concat(size_t src_a_count, char* src_a, size_t src_b_count, char* src_b, size_t dest_count, char* dest) {
     for (int i = 0; i < src_a_count; i++) {
         *dest++ = *src_a++;
     }
@@ -27,12 +25,11 @@ string_concat(size_t src_a_count, char* src_a, size_t src_b_count, char* src_b, 
     *dest++ = 0;
 }
 
-int 
-string_len(char* string) {
+int string_len(char* string) {
     int result = 0;
     
     // *string dereference value
-    // string++ advance pointer
+    // string++ advances pointer
     // search for null terminator
     while (*string++ != 0) {
         result++;
@@ -40,3 +37,30 @@ string_len(char* string) {
     
     return result;
 }
+
+int string_to_binary(char* str) {
+	int size = string_len(str);
+	int result = 0;
+	char* ptr = str;
+	int i = 0;
+    
+	while (*ptr) {
+		i++;
+		if (*ptr == '1') {
+			result |= (1 << (size - i));
+		}
+		ptr++;
+	}
+    
+	return result;
+}
+
+char* int_to_string(int n) {
+	static char binary[8];
+	for (auto x = 0; x < 8; x++) {
+		binary[x] = n & 0x80 ? '1' : '0';
+		n <<= 1;
+	}
+	return(binary);
+}
+
