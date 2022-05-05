85194f0b0c98c0b8e3a5239f11fb23c4c82b3c3c
Romanas <gensatus@gmail.com>
2022-05-05 13:14:23 +0300

    end of day 101

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,4 @@
-// https://www.youtube.com/watch?v=PHz3ulc_mFk
+// https://youtu.be/PPDAqEJvUUQ?t=2025
 // there is some bug which was introduced on day 78 with bottom stairs not having collision
 
 #include <stdio.h>
